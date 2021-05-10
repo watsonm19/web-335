8629efb5e10b73e4a371c89f917bfc48b19cb0d9
watsonm19 <watsonm19@me.com>
2021-05-09 17:24:55 -0700

    add attribution to 8.2, add exercise 8.3 - python in action

---
+++ week-8/watson_calculator.py
@@ -0,0 +1,21 @@
+# ===============================================
+# Title: Exercise 8.3 - Python In Action
+# Author: Professor Krasso
+# Date: 9 May 2021
+# Modified By: Mark Watson
+# Description: This program shows how to create
+#    functions and print results in python
+# ===============================================
+
+def add(a, b):
+  return a + b
+
+def subtract(x, y):
+  return x - y
+
+def divide(j, k):
+  return j / k
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
--- week-8/watson_hello_world.py
+++ week-8/watson_hello_world.py
@@ -1,1 +1,10 @@
+# ===============================================
+# Title: Exercise 8.2
+# Author: Professor Krasso
+# Date: 9 May 2021
+# Modified By: Mark Watson
+# Description: This program shows how to print
+#   a custom message in python.
+# ===============================================
+
 print('Hello World')
